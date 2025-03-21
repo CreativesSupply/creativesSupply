54f498bc93588bf20646d93546754bcd2e635211
Akmal <>
2025-03-21 20:28:32 +0800

    latest

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 var CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbz07Oi4uot37a59ZYaf6ETzT8S3fr2lR_Zg-2LKrs_nX9itmj7r4vqNsyqjcHtecVZMCA/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbyoUwp1Hklj6latkGx63yZ_w8AgkzkbqFyzKvUnN4MOzBYrAhz3ylYzQMdtQn5MoxpfLQ/exec"
 };
