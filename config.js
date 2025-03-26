31490a153a0f7c61280e263414e510c8da37a625
sayaCreativeDev <hello@creativessupply.com>
2025-03-27 03:23:42 +0800

    able update bookings

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 var CONFIG = {
-    API_URL: "https://script.google.com/macros/s/AKfycbyoUwp1Hklj6latkGx63yZ_w8AgkzkbqFyzKvUnN4MOzBYrAhz3ylYzQMdtQn5MoxpfLQ/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbyBgAI0TT-Vp4lC8nZiYzt5mCHu4kmF9Ytprs39R85yablRedyw-w11dnlHwYntDLBOgw/exec"
 };
